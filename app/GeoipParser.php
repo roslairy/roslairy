09eee4b29e4c36b15f543cc365318e8262bd9475
roslairy crimro <roslairy crimro>
2016-02-14 13:04:15 +0800

    1.1.3 修复GEOIP获取失败BUG

--- app/GeoipParser.php
+++ app/GeoipParser.php
@@ -5,19 +5,22 @@ use GeoIp2\Database\Reader;
 use GeoIp2\Exception\AddressNotFoundException;
 class GeoipParser{
 	public static function parse($ip){
-		
+
 		$reader = new Reader(storage_path("geoip/city.mmdb"));
-		
+
 		$address = "";
-		
+
 		try {
 			$record = $reader->city($ip);
 			$address = $record->country->names["zh-CN"]."-".$record->city->names["zh-CN"];
-		} 
+		}
 		catch (AddressNotFoundException $e) {
 			$address = "无记录";
 		}
-		
+		catch (ErrorException $e) {
+			$address = $record->country->name."-".$record->city->name;
+		}
+
 		return $address;
 	}
 }
